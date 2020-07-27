35b45ea6a5a44c1893fd9a73b6c585137a97fd42
Tuomo Sirén <tuomo.siren@gmail.com>
2020-07-28 00:22:22 +0300

    Typo fixes

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -euxo
 
-ssh etf@et2.org mkdir /tmp/etfdeploy
+ssh etf@etf2.org mkdir /tmp/etfdeploy
 scp $TRAVIS_BUILD_DIR/*.7z etf@etf2.org:/tmp/etfdeploy
 ssh etf@etf2.org 7z x /tmp/etfdeploy/*.7z
 ssh etf@etf2.org cp /tmp/etfdeploy/et-ded.x86 ~/wolfet
