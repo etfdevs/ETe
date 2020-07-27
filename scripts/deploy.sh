6f467e0a759a2de39f056c5289451f4c3c57d18c
Tuomo Sirén <tuomo.siren@gmail.com>
2020-07-28 00:37:17 +0300

    Try hard

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -euxo
+#set -euxo
 
 ssh etf@etf2.org mkdir /tmp/etfdeploy
 scp $TRAVIS_BUILD_DIR/*.7z etf@etf2.org:/tmp/etfdeploy
 ssh etf@etf2.org 7z x /tmp/etfdeploy/*.7z
-ssh etf@etf2.org cp /tmp/etfdeploy/et-ded.x86 ~/wolfet
+ssh etf@etf2.org cp /tmp/etfdeploy/ete-ded.x86 ~/wolfet
 ssh etf@etf2.org rm -rf /tmp/etfdeploy
 ssh etf@etf2.org sudo systemctl restart etf.service
 
-echo success
+echo "Deployment successful"
